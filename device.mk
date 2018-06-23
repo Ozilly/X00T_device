126908b06414498c6c23d3f4f18512648866ac94
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    X00T: Build separate system and vendor hidl lib
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    Signed-off-by: Chaitanaya-Mittal <chaitanayamittal98@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- device.mk
+++ device.mk
@@ -343,7 +343,7 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0_system
 
 #PRODUCT_ENFORCE_RRO_TARGETS := \
 #    framework-res
